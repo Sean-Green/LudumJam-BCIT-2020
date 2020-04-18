6f859677a76ecbfae9d5540d066d49a8d3bbc76d
Sean <sean.joseph.green@gmail.com>
2020-04-18 11:00:21 -0700

    Prototype with simple movement, collision, and NPC interaction.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (Input.GetKeyDown("space")){
+            col.gameObject.GetComponent<Script>().Interact();
+        }
+    }
+    
+}
