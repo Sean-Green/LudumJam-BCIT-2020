851d54c4704eb396b91b847b5edb0d97ba9b8b0c
Tate Smith <tate.smith.4242@gmail.com>
2020-04-19 16:21:09 -0700

    fixed diagonal wall movement

--- Assets/Scripts/GridSnapper.cs
+++ Assets/Scripts/GridSnapper.cs
@@ -33,16 +33,26 @@ public class GridSnapper : MonoBehaviour
 
         //TODO: do not let diagonal movement happen if the edge piece is an obstacle
         if (Vector3.Distance(transform.position, v2GoToPos) == 0f) {
-            if (!Physics2D.OverlapCircle(v2GoToPos + new Vector2(nMoveX * fGridSize, 0), 0.2f, lmCollideWith)) {
-                if (Mathf.Abs(nMoveX) == 1) {
+
+            if (Mathf.Abs(nMoveX) == 1 && Mathf.Abs(nMoveY) == 1)
+                if (!(Physics2D.OverlapCircle(v2GoToPos + new Vector2(nMoveX * fGridSize, 0), 0.2f, lmCollideWith)
+                    || Physics2D.OverlapCircle(v2GoToPos + new Vector2(0, nMoveY * fGridSize), 0.2f, lmCollideWith)
+                    || Physics2D.OverlapCircle(v2GoToPos + new Vector2(nMoveX * fGridSize, nMoveY * fGridSize), 0.2f, lmCollideWith))) {
+                    v2GoToPos += new Vector2(nMoveX * fGridSize, nMoveY * fGridSize);
+                } else if (!Physics2D.OverlapCircle(v2GoToPos + new Vector2(nMoveX * fGridSize, 0), 0.2f, lmCollideWith)) {
                     v2GoToPos += new Vector2(nMoveX * fGridSize, 0);
-                }
-            }
-            if (!Physics2D.OverlapCircle(v2GoToPos + new Vector2(0, nMoveY * fGridSize), 0.2f, lmCollideWith)) {
-                if (Mathf.Abs(nMoveY) == 1) {
+                } else if (!Physics2D.OverlapCircle(v2GoToPos + new Vector2(0, nMoveY * fGridSize), 0.2f, lmCollideWith)) {
                     v2GoToPos += new Vector2(0, nMoveY * fGridSize);
                 }
-            }
+                    
+
+            if (Mathf.Abs(nMoveX) == 1 && nMoveY == 0)
+                if (!Physics2D.OverlapCircle(v2GoToPos + new Vector2(nMoveX * fGridSize, 0), 0.2f, lmCollideWith))
+                    v2GoToPos += new Vector2(nMoveX * fGridSize, 0);
+
+            if (Mathf.Abs(nMoveY) == 1 && nMoveX == 0)
+                if (!Physics2D.OverlapCircle(v2GoToPos + new Vector2(0, nMoveY * fGridSize), 0.2f, lmCollideWith))
+                    v2GoToPos += new Vector2(0, nMoveY * fGridSize);
         }
 
         transform.position = Vector3.MoveTowards(transform.position, v2GoToPos, fMoveSpeed * Time.deltaTime);
